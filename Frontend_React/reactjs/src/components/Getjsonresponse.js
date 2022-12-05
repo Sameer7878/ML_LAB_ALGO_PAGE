11588a65d8ae10d6c85767cef7cdc3392aa21c89
vishnu teja reddy <vishnuteja779986@gmail.com>
2022-12-05 23:07:17 +0530

    updated react JsonGetResponse

---
+++ Frontend_React/reactjs/src/components/Getjsonresponse.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Getjsonresponse = (props) => {
+
+    const choosealogjson = props.value
+    return(<div>
+        <h1>hello</h1>
+        <p>{choosealogjson}</p>
+    </div>)
+
+}
+
+export default Getjsonresponse;
